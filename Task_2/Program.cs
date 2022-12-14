0a9b3a4a5cd91fbb1e7ba17de94451252a553b95
lokman <kalinin5000@yandex.ru>
2022-12-14 20:14:56 +0300

    решение второй\ задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите значение b1.");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1.");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2.");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2.");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+int x = (-b2 + b1)/(-k1 + k2);
+int y = k2 * x + b2;
+
+Console.WriteLine($"Прямые пересекутся в точке с координатами ({x}; {y})");
